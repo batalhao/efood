c510e5182565abb261f720fb5c00fab480e89eaf
Paulo R. Batalhão <paulo.batalhao@javamail.com.br>
2020-09-27 20:11:03 +0100

    Added JWT and Keycloak

--- cadastro/src/test/java/com/batalhao/efood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/batalhao/efood/cadastro/RestauranteResourceTest.java
@@ -3,8 +3,11 @@ package com.batalhao.efood.cadastro;
 import javax.ws.rs.core.Response.Status;
 import org.approvaltests.Approvals;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.batalhao.efood.cadastro.dto.AtualizarRestauranteDTO;
 import com.batalhao.efood.cadastro.model.Restaurante;
+import com.batalhao.efood.cadastro.util.TokenUtils;
 import com.github.database.rider.cdi.api.DBRider;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
@@ -13,6 +16,7 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.specification.RequestSpecification;
 
 @DBRider
@@ -21,8 +25,16 @@ import io.restassured.specification.RequestSpecification;
 @QuarkusTestResource(CadastroTestLifecycleManager.class)
 public class RestauranteResourceTest {
 
+  private String token;
+
+  @BeforeEach
+  public void gereToken() throws Exception {
+    token = TokenUtils.generateTokenString("/JWTProprietarioClaims.json", null);
+  }
+
   private RequestSpecification given() {
-    return RestAssured.given().contentType(ContentType.JSON);
+    return RestAssured.given().contentType(ContentType.JSON)
+        .header(new Header("Authorization", "Bearer " + token));
   }
 
   @Test
@@ -37,15 +49,15 @@ public class RestauranteResourceTest {
   @Test
   @DataSet("restaurante-cenario-1.yml")
   public void testaAlterarRestaurante() {
-    Restaurante dto = new Restaurante();
-    dto.nome = "Novo nome";
+    AtualizarRestauranteDTO dto = new AtualizarRestauranteDTO();
+    dto.nomeFantasia = "Novo nome";
     Long parameterValue = 123L;
     given().with().pathParam("id", parameterValue).body(dto).when().put("/restaurantes/{id}").then()
         .statusCode(Status.NO_CONTENT.getStatusCode()).extract().asString();
 
     Restaurante findById = Restaurante.findById(parameterValue);
     // Assert.assertEquals(dto.nome, findById.nome);
-    Assert.assertEquals(dto.nome, findById.nome);
+    Assert.assertEquals(dto.nomeFantasia, findById.nome);
   }
 
 }
