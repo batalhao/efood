63f3d8ac2bcd855bbf47faa00f495802b49a2f6d
Paulo R. Batalhão <paulo.batalhao@javamail.com.br>
2020-10-03 19:12:20 +0100

    Added persistence and to String

--- marketplace/src/main/java/com/batalhao/efood/mp/model/Restaurante.java
+++ marketplace/src/main/java/com/batalhao/efood/mp/model/Restaurante.java
@@ -1,5 +1,8 @@
 package com.batalhao.efood.mp.model;
 
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Tuple;
+
 public class Restaurante {
 
   public Long id;
@@ -7,5 +10,19 @@ public class Restaurante {
   public String nome;
 
   public Localizacao localizacao;
+
+  public void persist(PgPool pgPool) {
+    pgPool.preparedQuery("insert into localizacao (id, latitude, longitude) values ($1, $2, $3)")
+        .execute(Tuple.of(localizacao.id, localizacao.latitude, localizacao.longitude)).await()
+        .indefinitely();
+
+    pgPool.preparedQuery("insert into restaurante (id, nome, localizacao_id) values ($1, $2, $3)")
+        .execute(Tuple.of(id, nome, localizacao.id)).await().indefinitely();
+  }
+
+  @Override
+  public String toString() {
+    return "Restaurante [id=" + id + ", nome=" + nome + ", localizacao=" + localizacao + "]";
+  }
 
 }
